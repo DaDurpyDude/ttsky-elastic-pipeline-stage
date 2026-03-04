5bf38140c0f841af1514d83d4cfbf9efb4487e7f
Rishikesh Sethuraman <68036190+DaDurpyDude@users.noreply.github.com>
2026-03-04 02:09:18 +0000

    fixing read/valid for instant values

--- src/pipeline_skid_buffer.v
+++ src/pipeline_skid_buffer.v
@@ -1,58 +1,46 @@
-/*
- * Copyright (c) 2024 Rishikesh Sethuraman
- * SPDX-License-Identifier: Apache-2.0
- */
-
- `default_nettype none
+`default_nettype none
 
 module pipeline_skid_buffer
-
-  #(parameter datapath_gate_p = 0
+  #(parameter datapath_gate_p  = 0
    ,parameter datapath_reset_p = 0
    )
-
-  (input clk_i
-  ,input reset_i
-  ,output ready_o
-  ,input valid_i
-  ,input [7:0] data_i 
-  ,input ready_i
-  ,output valid_o 
-  ,output [7:0] data_o 
+  ( input  wire       clk_i
+  , input  wire       reset_i
+  , output wire       ready_o
+  , input  wire       valid_i
+  , input  wire [7:0] data_i
+  , input  wire       ready_i
+  , output wire       valid_o
+  , output wire [7:0] data_o
   );
 
   reg [7:0] data_l;
-  reg valid_l;
+  reg       valid_l;
 
-  always @(posedge clk_i) begin
+  wire ready_pre;
+  assign ready_pre = ready_i | ~valid_l;
+
+  always @(posedge clk_i or posedge reset_i) begin
     if (reset_i) begin
       valid_l <= 1'b0;
       if (datapath_reset_p) begin
         data_l <= 8'b0;
       end
-    end else begin
-      if (ready_o & valid_i) begin
-        valid_l <= 1'b1;
-      end else begin
-        if (ready_i) begin
-          valid_l <= 1'b0;
-        end
-      end
+    end else if (ready_pre) begin
+      valid_l <= valid_i;
+
       if (datapath_gate_p) begin
-        if (ready_o & valid_i) begin
+        if (valid_i) begin
           data_l <= data_i;
         end
       end else begin
-        if (ready_o) begin
-          data_l <= data_i;
-        end
+        data_l <= data_i;
       end
     end
   end
-  
-  assign data_o = data_l;
-  assign ready_o = ready_i | ~valid_o;
+
   assign valid_o = valid_l;
+  assign data_o  = data_l;
+  assign ready_o = ready_i | ~valid_l;
 
 endmodule
-
