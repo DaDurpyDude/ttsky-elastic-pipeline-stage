123635c6366759a1432c8220976639d7c2821762
Rishikesh Sethuraman <68036190+DaDurpyDude@users.noreply.github.com>
2026-03-04 02:22:32 +0000

    rtl fix

--- src/pipeline_skid_buffer.v
+++ src/pipeline_skid_buffer.v
@@ -17,25 +17,26 @@ module pipeline_skid_buffer
   reg [7:0] data_l;
   reg       valid_l;
 
-  wire ready_pre;
-  assign ready_pre = ready_i | ~valid_l;
+  // Ready computation: ready_pre is high if the buffer is empty or downstream is ready
+  wire ready_pre = ready_i | ~valid_l;
 
   always @(posedge clk_i or posedge reset_i) begin
     if (reset_i) begin
       valid_l <= 1'b0;
-      if (datapath_reset_p) begin
+      if (datapath_reset_p)
         data_l <= 8'b0;
+      // Otherwise don't touch data_l per datapath_reset_p=0
+    end else begin
+      if (ready_pre && valid_i) begin
+        // True transfer: accept new data
+        valid_l <= 1'b1;
+        data_l  <= data_i;
+      end else if (ready_i) begin
+        // Buffer drains: clear valid if downstream accepts data
+        valid_l <= 1'b0;
+        // NOTE: data_l should NOT be updated here or else you break stall correctness
       end
-    end else if (ready_pre) begin
-      valid_l <= valid_i;
-
-      if (datapath_gate_p) begin
-        if (valid_i) begin
-          data_l <= data_i;
-        end
-      end else begin
-        data_l <= data_i;
-      end
+      // Else (no transfer, no readout): hold valid_l, data_l
     end
   end
 
