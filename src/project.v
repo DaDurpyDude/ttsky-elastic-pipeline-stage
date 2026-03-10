308cdb5b78c59bc7d47f48d8c8d1b47e6729dc32
Rishikesh Sethuraman <68036190+DaDurpyDude@users.noreply.github.com>
2026-03-10 22:35:31 +0000

    fix tb module

--- src/project.v
+++ src/project.v
@@ -6,6 +6,10 @@
 `default_nettype none
 
 module tt_um_pipeline_skid_buffer (
+`ifdef USE_POWER_PINS
+    inout vccd1,
+    inout vssd1,
+`endif
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
--- test/tb.v
+++ test/tb.v
@@ -1,19 +1,13 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 module tb ();
-
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
     #1;
   end
 
-  // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
   reg ena;
@@ -23,7 +17,19 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
+  // Power supplies required for GL simulation with USE_POWER_PINS.
+  // sky130 cells have explicit VPWR/VGND ports when this flag is set.
+  // Without these, every FF powers up with X and never resolves.
+`ifdef GL_TEST
+  supply1 vccd1;
+  supply0 vssd1;
+`endif
+
   tt_um_pipeline_skid_buffer user_project (
+`ifdef GL_TEST
+      .vccd1  (vccd1),
+      .vssd1  (vssd1),
+`endif
       .ui_in  (ui_in),
       .uo_out (uo_out),
       .uio_in (uio_in),
